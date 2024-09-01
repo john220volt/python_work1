f164be5a740a4df2a1007d060284ddcedca932c3
Evgeni Suhinin <sewsk-boy@yandex.ru>
2024-09-01 14:57:06 +0300

    fff

--- proba_pera.py
+++ proba_pera.py
@@ -4,3 +4,14 @@ def sum_nambers(n):
         summa += i
     print (summa)
 sum_nambers(5)
+
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#print(list_1[1])
+#print(list_1[:3])
+#print(list_1[2:])
+#print(list_1.insert(5, 4))
+print(list_1[len(list_1)-1])
+print(list_1 [len(list_1)-2:])
+
+t =  (1, 2, 3,)
+print(type(t))
